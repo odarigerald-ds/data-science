0505a083659d3780754236af4e797eb7179c99c1
Odari Gerald <125037918+odarigerald-ds@users.noreply.github.com>
2023-02-10 22:49:24 +0300

    added variable in Python file
    Added a Python file: an exercise on variables. Worked with strings and concatenation of strings to print out my full names from different names stored in different variables.

---
+++ Python/Variables in Python.py
@@ -0,0 +1,24 @@
+#Exercise: Python Variables
+#Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+
+#break = 5
+
+#Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+birth_year = 1993
+current_year = 2023
+age = current_year - birth_year
+print("My age is", age)
+
+#Store your first, middle and last name in three different variables and then print your full name using these variables
+
+first_name = "Odari"
+middle_name = "Gerald"
+last_name = "Angule"
+
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)
+
+#Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+# Invalid variable names are: 1record record^one continue
+
